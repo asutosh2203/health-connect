d535072bac7862243a56f786fdfed2f9f7a4a719
Asutosh Padhi <asutosh2001@gmail.com>
2024-08-27 02:28:23 +0530

    home page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">Health Connect</main>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification for Admin */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 HealthConnect
+            </p>
+            <Link href={"/admin=true"} className="text-green-500 underline">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
 }
